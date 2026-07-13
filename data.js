bfccec4f36acfd71177f5c860d5eb8782a397ea0
mocha-sahara <syamhud717@gmail.com>
2026-07-13 21:29:51 +0700

    Update data.js

--- data.js
+++ data.js
@@ -38,7 +38,7 @@ const dataUndangan = {
             tanggalCountdown: "2026-08-02T08:00:00", 
             waktu: "08:00 WIB - 10:00 WIB",
             tempat: "Kediaman Mempelai Wanita",
-            alamatLengkap: "Dsn. Balongrejo, Ds. Balonggebang, RT 30, RW 10, Kec. Gondang, Kab. Nganjuk",
+            alamatLengkap: "Balongrejo - Balonggebang - Gondang - Nganjuk",
             linkGoogleMaps: "https://goo.gl/maps/1Dvg7RLgNhKyL9bs8?g_st=ac"
         },
         resepsi: {
